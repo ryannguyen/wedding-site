e3c1ee30ed82950d59ff7aefba6fbc436ca12587
Ryan Nguyen <rnguyen@visualsupply.co>
2013-03-08 23:39:21 -0800

    forgot all the files

---
+++ public/js/collections.js
@@ -0,0 +1,44 @@
+/**
+    COLLECTIONS
+ */
+
+var Wedding = (window.Wedding = window.Wedding || {});
+
+(function(App) {
+    App.Invitations = Backbone.Collection.extend({
+        adultCount: 0,
+        childrenCount: 0,
+        toddlerCount: 0,
+        infantCount: 0,
+        url: '/api/invitations',
+        model: App.Invitation,
+        initialize: function() {
+            this.count();
+            this.listenTo(this, 'reset', this.count);
+        },
+        comparator: function(model) {
+            return model.get('label').toLowerCase();
+        },
+        count: function() {
+            var _this = this;
+            this.each(function(model) {
+                model.people.each(function(person) {
+                    if(person.get('type') == "adult" )
+                        _this.adultCount = _this.adultCount + 1;
+                    if(person.get('type') == "child" )
+                        _this.childrenCount = _this.childrenCount + 1;
+                    if(person.get('type') == "infant" || person.get('type') == "Infant" )
+                        _this.infantCount = _this.infantCount + 1;
+                    if(person.get('type') == "toddler" )
+                        _this.toddlerCount = _this.toddlerCount + 1;
+
+                });
+            });
+        }
+    });
+
+    App.People = Backbone.Collection.extend({
+        model: App.Person
+    });
+
+})(Wedding);
---
+++ public/js/models.js
@@ -0,0 +1,64 @@
+/**
+ * MODELS
+*/
+var Wedding = (window.Wedding = window.Wedding || {});
+
+(function(App) {
+
+    App.Invitation = Backbone.Model.extend({
+        defaults: {
+            address: '',
+            side: ''
+        },
+        initialize: function(attr, options) {
+            attr = attr || {};
+
+            if(attr.people) {
+                this.people = new App.People(attr.people);
+            } else {
+                this.people = new App.People;
+            }
+
+            if(!attr.modified_date)
+                this.set('modified_date', null);
+
+            if(attr._id) {
+                this.set('id', attr._id);
+            }
+        },
+        _setPeople: function() {
+            var people = [];
+            this.people.each(function(person) {
+                people.push(person.attributes);
+            });
+
+            this.set('people', people);
+            return this;
+        },
+        _generatePassword: function() {
+            if(this.get('password')) return;
+
+            var text = "";
+
+            // no i or l to avoid confusion
+            var possible = "ABCDEFGHJKLMNOPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz0123456789";
+
+            for( var i=0; i < 5; i++ )
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            this.set('password', text)
+            return this;
+        },
+        prep: function() {
+            this._setPeople()._generatePassword();
+            return this;
+        },
+        clear: function() {
+            Backbone.Model.prototype.clear.apply(this, arguments);
+            this.people.reset(null);
+        }
+
+    });
+
+    App.Person = Backbone.Model.extend({});
+})(Wedding);
